69fd8edd98c29a9cd546f867fef65741d73f370d
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-11-11 21:10:02 +0200

    Update mx_output_for_F

--- src/mx_output_for_F.c
+++ src/mx_output_for_F.c
@@ -4,28 +4,26 @@ void mx_output_for_F(mode_t mode)
 {
     mode_t modified_mode = mode & S_IFMT;
 
-    if (modified_mode == S_IFDIR)
-    {
-        mx_printchar('/');
-    }
-    else if (modified_mode == S_IFLNK)
-    {
-        mx_printchar('@');
-    }
-    else if (modified_mode == S_IFSOCK)
-    {
-        mx_printchar('=');
-    }
-    else if (modified_mode == S_IFIFO)
-    {
-        mx_printchar('|');
-    }
-    else if (IS_X(mode) && IS_R(mode))
-    {
-        mx_printchar('*');
-    }
-    else if (modified_mode == S_IFWHT)
-    {
-        mx_printchar('%');
+    switch (modified_mode) {
+        case S_IFDIR:
+            mx_printchar('/');
+            break;
+        case S_IFLNK:
+            mx_printchar('@');
+            break;
+        case S_IFSOCK:
+            mx_printchar('=');
+            break;
+        case S_IFIFO:
+            mx_printchar('|');
+            break;
+        case S_IFWHT:
+            mx_printchar('%');
+            break;
+        default:
+            if (IS_X(mode) && IS_R(mode)) {
+                mx_printchar('*');
+            }
+            break;
     }
 }
