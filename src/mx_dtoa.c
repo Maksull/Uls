98a37abc9f6fb7cb1c7da40322c3a71c91f918a2
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-11-11 20:40:10 +0200

    Update mx_dtoa

--- src/mx_dtoa.c
+++ src/mx_dtoa.c
@@ -1,30 +1,33 @@
 #include "../inc/uls.h"
 
-char *mx_dtoa(float d, int precision)
-{
-    float tmp = d;
-    for (int i = 0; i < precision; i++)
-    {
-        tmp *= 10;
-    }
-
-    char *d_str = mx_itoa((int)tmp);
-
-    mx_realloc(d_str, 1);
-    int len = mx_strlen(d_str);
-
-    for (int i = len + 1; i >= len - precision; i--)
-    {
-        if (i == len - precision)
-        {
+static void insert_decimal_point(char* d_str, int precision, int len) {
+    for (int i = len + 1; i >= len - precision; i--) {
+        if (i == len - precision) {
             d_str[i] = '.';
-            
             break;
-        } else 
-        {
+        } else {
             d_str[i] = d_str[i - 1];
         }
     }
+}
+
+char *mx_dtoa(float d, int precision) {
+    float tmp = d;
     
+    // Multiply by 10^precision
+    for (int i = 0; i < precision; i++) {
+        tmp *= 10;
+    }
+
+    // Convert to string
+    char *d_str = mx_itoa((int)tmp);
+    int len = mx_strlen(d_str);
+
+    // Reallocate memory to accommodate the decimal point
+    d_str = mx_realloc(d_str, len + precision + 2);
+
+    // Insert the decimal point
+    insert_decimal_point(d_str, precision, len);
+
     return d_str;
 }
