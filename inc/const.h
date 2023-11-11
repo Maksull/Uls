03cc8cc19117592a5b19ed5a4ca29dc6fd0f17f9
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-11-11 21:46:49 +0200

    Update comments for headers

--- inc/const.h
+++ inc/const.h
@@ -1,34 +1,36 @@
 #ifndef ULS_CONST_H
 #define ULS_CONST_H
 
-static const int TAB_SIZE = 8;
-static const int CAT_WIDTH = 80;
-static const int SIX_MON_LEN = 15552000;
-#define XATTR_LEN 10000
+// Constants related to formatting and file attributes
+static const int TAB_SIZE = 8;            // Size of a tab character in spaces
+static const int CAT_WIDTH = 80;          // Maximum width for formatted output
+static const int SIX_MON_LEN = 15552000;  // Length of six months in seconds
+#define XATTR_LEN 10000                    // Maximum length for extended attribute data
 
-static const int FLAG_COUNT = 19;
-static const char const_uls_flags[] = { 'a', 'A', 'G', 'l', 'r', 't', 'c', 'u', 'S', 'R', 
-                                    'T', 'h', '@', 'e', 'C', '1', 'F', 'f', 'm' };
+// Constants related to command-line flags and usage messages
+static const int FLAG_COUNT = 19;         // Total number of supported flags
+static const char const_uls_flags[] = { 'a', 'A', 'G', 'l', 'r', 't', 'c', 'u', 'S', 'R',
+                                         'T', 'h', '@', 'e', 'C', '1', 'F', 'f', 'm' };
+                                         
+static const char* USAGE_STR    = "usage: uls [-ACFGRSTacefhlmrtu1] [file ...]\n";  // Usage message
+static const char* ULS_STR      = "uls: ";  // Prefix for uls-related messages
+static const char* ILOP_STR     = "illegal option -- ";  // Error message for illegal option
+static const char* EXE_STR      = "uls";  // Executable name
 
-static const char* USAGE_STR    = "usage: uls [-ACFGRSTacefhlmrtu1] [file ...]\n";
-static const char* ULS_STR      = "uls: ";
-static const char* ILOP_STR     = "illegal option -- ";
-static const char* EXE_STR      = "uls";
+// Constants for color codes
+static const char* COLOR_DEFAULT        = "\033[0m";  // Default color
 
-// Colors
-static const char* COLOR_DEFAULT        = "\033[0m";
+static const char* COLOR_RED_DFLT       = "\033[0;31m";  // Red
+static const char* COLOR_GREEN_DFLT     = "\033[0;32m";  // Green
+static const char* COLOR_YELLOW_DFLT    = "\033[0;33m";  // Yellow
+static const char* COLOR_BLUE_DFLT      = "\033[0;34m";  // Blue
+static const char* COLOR_MAGENTA_DFLT   = "\033[0;35m";  // Magenta
 
-static const char* COLOR_RED_DFLT       = "\033[0;31m";
-static const char* COLOR_GREEN_DFLT     = "\033[0;32m";
-static const char* COLOR_YELLOW_DFLT    = "\033[0;33m";
-static const char* COLOR_BLUE_DFLT      = "\033[0;34m";
-static const char* COLOR_MAGENTA_DFLT   = "\033[0;35m";
-
-static const char* COLOR_BLUE_CYAN      = "\033[46;34m";
-static const char* COLOR_BLUE_YELLOW    = "\033[43;34m";
-static const char* COLOR_BLACK_RED      = "\033[41;30m";
-static const char* COLOR_BLACK_CYAN     = "\033[46;30m";
-static const char* COLOR_BLACK_GREEN    = "\033[42;30m";
-static const char* COLOR_BLACK_YELLOW   = "\033[43;30m";
+static const char* COLOR_BLUE_CYAN      = "\033[46;34m";  // Cyan background, blue text
+static const char* COLOR_BLUE_YELLOW    = "\033[43;34m";  // Yellow background, blue text
+static const char* COLOR_BLACK_RED      = "\033[41;30m";  // Red background, black text
+static const char* COLOR_BLACK_CYAN     = "\033[46;30m";  // Cyan background, black text
+static const char* COLOR_BLACK_GREEN    = "\033[42;30m";  // Green background, black text
+static const char* COLOR_BLACK_YELLOW   = "\033[43;30m";  // Yellow background, black text
 
 #endif
--- inc/uls.h
+++ inc/uls.h
@@ -1,6 +1,7 @@
 #ifndef ULS_H
 #define ULS_H
 
+// Standard C Library Headers
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/xattr.h>
@@ -12,70 +13,69 @@
 #include <time.h>
 #include <string.h>
 #include <errno.h>
+
+// Custom Library Headers
 #include "../libmx/inc/libmx.h"
 #include "const.h"
 
+// Definition of structures for handling file information and command-line flags
 typedef struct dirent t_dirent;
 typedef struct stat t_stat;
 typedef struct passwd t_passwd;
 typedef struct group t_group;
 
 typedef struct s_uls_flags {
-    int a;
-    int A;
-    int G;
-    int l;
-    int r;
-    int t;
-    int c;
-    int u;
-    int S;
-    int T;
-    int h;
-    int at;
-    int e;
-    int R;
-    int C;
-    int one;
-    int F;
-    int f;
-    int m;
+    int a;   // Show hidden files
+    int A;   // Show hidden files, excluding '.' and '..'
+    int G;   // Enable colorized output
+    int l;   // List format
+    int r;   // Reverse order
+    int t;   // Sort by modification time
+    int c;   // Use time of last status change for sorting
+    int u;   // Use time of last access for sorting
+    int S;   // Sort by file size
+    int T;   // Display complete time information
+    int h;   // Human-readable sizes
+    int at;  // Display file type symbol after name
+    int e;   // Print Access Control List (ACL)
+    int R;   // Recursively list subdirectories encountered
+    int C;   // Force multi-column output
+    int one; // Force output to be one entry per line
+    int F;   // Append indicator (one of */=@|) to entries
+    int f;   // Disable sorting; display entries in directory order
+    int m;   // Stream output format; list files across the page
 } t_uls_flags;
 
 typedef struct s_file {
-    char* num_links;
-    char* user_name;
-    char* group_name;
-    char* size;
-    char* lm_day;
-    char* lm_time;
-    char* lm_month;
-    char* perms;
-    char* linked_file;
-    char* acl_str;
+    char* num_links;      // Number of hard links
+    char* user_name;      // Name of the file owner
+    char* group_name;     // Name of the file group
+    char* size;           // Size of the file
+    char* lm_day;         // Last modification day
+    char* lm_time;        // Last modification time
+    char* lm_month;       // Last modification month
+    char* perms;          // File permissions
+    char* linked_file;    // Linked file (for symbolic links)
+    char* acl_str;        // Access Control List (ACL) string
 
-    char* name;
-    char* path;
-    char type;
-    t_stat stat;
+    char* name;           // File name
+    char* path;           // Full path of the file
+    char type;            // File type
+    t_stat stat;          // File statistics
 
-    struct s_file* next;
+    struct s_file* next;  // Pointer to the next file in the linked list
 } t_file;
 
-
-
+// Definitions of macro functions for checking file attributes
 #define IS_R(m) (m & S_IRUSR && m & S_IRGRP && m & S_IROTH)
 #define IS_X(m) (m & S_IXUSR && m & S_IXGRP && m & S_IXOTH)
 #define IS_DIR(m) (((m) & 00170000) == 0040000)
 #define IS_LNK(m) (((m) & S_IFMT) == S_IFLNK)
 #define IS_REG(m) (((m) & S_IFMT) == S_IFREG)
 
-
-// FILE UTILITIES
-
+// Function prototypes for file utilities
 int mx_uls_init(char** files, int file_count, t_uls_flags* flags);
 bool mx_is_implied_dir(const char* dir_name);
-
 int mx_get_total_blocks(t_file** files);
 char* mx_get_file_permissions(t_file* file, t_uls_flags* flags);
 char mx_get_file_type(mode_t mode);
@@ -84,16 +84,13 @@ char* mx_get_file_size(long int f_size);
 void mx_get_file_lm_date(t_file **file, t_uls_flags* flags);
 char* mx_get_linked_file(t_file* file);
 
-// FILES & FILE ARRAY
-
+// Function prototypes for files and file array handling
 t_file* mx_create_file_obj(const char* path, const char* name, t_uls_flags* flags);
 t_file* mx_create_default_file_obj(const char* dir_name, const char* name);
-
 void mx_handle_file_array(t_file** files, const char* dir_name, bool is_dir, bool is_single, t_uls_flags* flags);
 int mx_handle_dir(const char* dir_name, t_uls_flags* flags, bool is_single);
 
-// PRINT FILE INFO
-
+// Function prototypes for printing file information
 void mx_output_files_l(t_file** files, bool is_dir, t_uls_flags* flags);
 void mx_output_file_l(t_file* file, t_uls_flags* flags);
 void mx_output_for_F(mode_t mode);
@@ -102,44 +99,38 @@ void mx_output_for_m(t_file** files, t_uls_flags* flags);
 void mx_print_dir_name(const char* dir_name);
 void mx_print_color_file_name(char* file_name, mode_t mode);
 void mx_output_acl_info(const char* acl_str);
-void mx_output_for_F(mode_t mode);
 void mx_output_file_xattr(const char* path, bool is_h_on);
 
-// FLAG UTIL
-
-t_uls_flags* mx_get_uls_flags(int argc,const char** argv, int* flag_count);
+// Function prototypes for flag utility functions
+t_uls_flags* mx_get_uls_flags(int argc, const char** argv, int* flag_count);
 bool mx_is_uls_flags_applied(t_uls_flags* flags, char* file_name);
 void mx_add_flag(t_uls_flags** flags, char flag);
 
-// ERROR HANDLING
-
+// Function prototypes for error handling
 void mx_print_flag_err(char flag);
 void mx_print_dir_error(const char* error, const char* dir_name);
 void mx_print_perm_dir_error(const char* error, const char* dir_path, bool is_single);
 
-// MISC 
-
+// Miscellaneous utility functions
 void mx_reset_file_params_length(t_file** files, t_uls_flags* flags);
 
-// LIST
-
-void mx_clear_list(t_file **list);
-void mx_push_back(t_file **list, const char* dir_name, const char* file_name, t_uls_flags* flags);
-int mx_list_size(t_file *list);
+// Function prototypes for linked list operations
+void mx_clear_list(t_file** list);
+void mx_push_back(t_file** list, const char* dir_name, const char* file_name, t_uls_flags* flags);
+int mx_list_size(t_file* list);
 t_file* mx_swap_nodes(t_file* node1, t_file* node2);
 
-// SORTING
-
+// Function prototypes for sorting
 void mx_sort_init(t_file** files, t_uls_flags* flags);
-void mx_sort_list(t_file **lst, bool (*cmp)(t_file* a, t_file* b));
+void mx_sort_list(t_file** lst, bool (*cmp)(t_file* a, t_file* b));
 void mx_reverse_list(t_file** list);
 bool compare_by_name(t_file* first, t_file* second);
 
-//UTILS
+// Miscellaneous utility functions
 float mx_round(float num, int precision);
-char *mx_dtoa(float d, int precision);
+char* mx_dtoa(float d, int precision);
 int mx_toupper(int c);
-void mx_printerr(const char *s);
-char *mx_strrchr(const char *s, int c);
+void mx_printerr(const char* s);
+char* mx_strrchr(const char* s, int c);
 
 #endif
