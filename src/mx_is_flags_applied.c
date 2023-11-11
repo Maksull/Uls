8849fce04d1549f89c586288c39322aae30f1101
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-11-11 20:55:21 +0200

    Update mx_is_uls_flags_applied

--- src/mx_is_flags_applied.c
+++ src/mx_is_flags_applied.c
@@ -5,13 +5,19 @@ static bool is_hidden_file(const char* name)
     return (name[0] == '.');
 }
 
+static bool should_skip_hidden_files(t_uls_flags* flags, const char* file_name) 
+{
+    return (!flags->a && !flags->A && (is_hidden_file(file_name) || mx_is_implied_dir(file_name)));
+}
+
+static bool should_skip_implied_dir(t_uls_flags* flags, const char* file_name) 
+{
+    return (flags->A && mx_is_implied_dir(file_name));
+}
+
 bool mx_is_uls_flags_applied(t_uls_flags* flags, char* file_name) 
 {
-    if (!flags->a && !flags->A && (is_hidden_file(file_name) || mx_is_implied_dir(file_name)))
-    {
-        return false;
-    }
-    if (flags->A && (mx_is_implied_dir(file_name)))
+    if (should_skip_hidden_files(flags, file_name) || should_skip_implied_dir(flags, file_name)) 
     {
         return false;
     }
