5c45a950d5b627fe2fa20f37668dd1df1799c45e
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2023-11-11 21:34:41 +0200

    Update mx_sort_list

--- src/mx_sort_list.c
+++ src/mx_sort_list.c
@@ -1,33 +1,64 @@
 #include "../inc/uls.h"
 
-void mx_sort_list(t_file** list, bool (*cmp)(t_file *a, t_file *b)) 
-{
-    t_file** file;
-    bool is_swapped;
-  
-    int count = mx_list_size(*list);
+static t_file* merge(t_file* left, t_file* right, bool (*cmp)(t_file* a, t_file* b)) {
+    t_file* result = NULL;
 
-    for (int i = 0; i < count; i++) {
-        file = list;
-        is_swapped = false;
-  
-        for (int j = 0; j < count - i - 1; j++) {
-  
-            t_file* f1 = *file;
-            t_file* f2 = f1->next;
-  
-            if (cmp(f1, f2)) {
-
-                *file = mx_swap_nodes(f1, f2);
-                is_swapped = true;
-            
-            }
-  
-            file = &(*file)->next;
-        }
-        if (!is_swapped)
-        {
-            break;
-        }
+    if (left == NULL) {
+        return right;
     }
+    if (right == NULL) {
+        return left;
+    }
+
+    if (cmp(left, right)) {
+        result = left;
+        result->next = merge(left->next, right, cmp);
+    } else {
+        result = right;
+        result->next = merge(left, right->next, cmp);
+    }
+
+    return result;
+}
+
+static void split(t_file* source, t_file** front, t_file** back) {
+    t_file* fast;
+    t_file* slow;
+
+    if (source == NULL || source->next == NULL) {
+        *front = source;
+        *back = NULL;
+    } else {
+        slow = source;
+        fast = source->next;
+
+        while (fast != NULL) {
+            fast = fast->next;
+            if (fast != NULL) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        *front = source;
+        *back = slow->next;
+        slow->next = NULL;
+    }
+}
+
+void mx_sort_list(t_file** list, bool (*cmp)(t_file* a, t_file* b)) {
+    t_file* head = *list;
+    t_file* left;
+    t_file* right;
+
+    if (head == NULL || head->next == NULL) {
+        return;
+    }
+
+    split(head, &left, &right);
+
+    mx_sort_list(&left, cmp);
+    mx_sort_list(&right, cmp);
+
+    *list = merge(left, right, cmp);
 }
