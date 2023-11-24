d7e578e37d49866a9eef89e2215805726aeff847
Maksull <49917345+Maksull@users.noreply.github.com>
2023-11-24 20:43:52 +0200

    Add comments

--- libmx/src/mx_itoa.c
+++ libmx/src/mx_itoa.c
@@ -1,46 +1,49 @@
 #include "libmx.h"
 
-static int mx_digit_count(int number)
-{
+// Function to count the number of digits in an integer
+static int mx_digit_count(int number) {
     int counter = 0;
 
-    do
-    {
+    // Count digits by continuously dividing the number by 10 until it becomes 0
+    do {
         number /= 10;
         ++counter;
     } while (number != 0);
 
-    return counter;
+    return counter; // Return the count of digits
 }
 
-char *mx_itoa(int number)
-{
+// Function to convert an integer to a string
+char *mx_itoa(int number) {
+    // Handling the special case of the minimum int value (-2147483648)
     if (number == -2147483648) {
         return mx_strdup("-2147483648");
     }
 
+    // Allocate memory for the resulting string based on the digit count of the number
     char *result = mx_strnew(mx_digit_count(number));
     int sign;
     
-    if ((sign = number) < 0)
-    {
+    // Handle the sign of the number
+    if ((sign = number) < 0) {
         number = -number;
     }
 
     int i = 0;
+    // Extract digits from the number and store them in the string
     do {
-        result[i++] = number % 10 + 48; 
+        result[i++] = number % 10 + 48; // Convert digit to character representation
     
     } while ((number /= 10) > 0);
     
-    if (sign < 0)
-    {
+    // Add '-' sign if the number was negative
+    if (sign < 0) {
         result[i++] = '-';
     }
     
-    result[i] = '\0';
+    result[i] = '\0'; // Add null terminator to mark the end of the string
 
-    mx_str_reverse(result);
+    mx_str_reverse(result); // Reverse the string to get the correct order of digits
     
-    return result;
+    return result; // Return the resulting string representation of the number
 }
--- src/mx_get_file_size.c
+++ src/mx_get_file_size.c
@@ -1,44 +1,47 @@
 #include "../inc/uls.h"
 
-static float round(float num, int precision)
-{
+// Function to round a floating-point number to a given precision
+static float round(float num, int precision) {
     float base = 1;
 
-    for (int i = 0; i < precision; i++)
-    {
+    // Multiply the base by 10 according to the precision required
+    for (int i = 0; i < precision; i++) {
         base *= 10;
     }
     
     num *= base;
 
+    // Rounding to the nearest integer value
     if (num < 0.0)
-        return ((int)(num - 0.5)) / base;
+        return ((int)(num - 0.5)) / base; // Adjusted downward
     else
-        return ((int)(num + 0.5) ) / base;
-    
+        return ((int)(num + 0.5) ) / base; // Adjusted upward
 }
 
+// Function to format the size string with appropriate units
 static char* format_size_str(float d, int precision, char* letter) {
-
     char *file_size_str;
     float tmp = d;
 
+    // Check if the value is greater than 10 to round to an integer
     if ((int)tmp / 10 > 0) {
-        tmp = round(tmp, 0);
-        file_size_str = mx_itoa(tmp);
-
+        tmp = round(tmp, 0); // Round to an integer if value is greater than 10
+        file_size_str = mx_itoa(tmp); // Convert integer to string
     } else {
+        // Round the value to the given precision
         tmp = round(tmp, precision);
-        file_size_str = mx_dtoa(tmp, precision);
+        file_size_str = mx_dtoa(tmp, precision); // Convert float to string
     }
 
+    // Concatenate the unit letter to the file size string
     mx_strcat(file_size_str, letter);
 
-    return file_size_str;
+    return file_size_str; // Return formatted file size string
 }
+
 // Function to get a human-readable file size string from a given size in bytes
-char* mx_get_file_size(long int f_size) 
-{
+char* mx_get_file_size(long int f_size) {
+    // Define constants for byte sizes in different units
     int Kbyte = 1024;
     int Mbyte = 1024 * 1024;
     int Gbyte = 1024 * 1024 * 1024;
@@ -49,35 +52,27 @@ char* mx_get_file_size(long int f_size)
     char *file_size_str;
     float tmp;
 
+    // Determine the appropriate unit for the file size and format accordingly
     if (file_size >= Pbyte) {
-
         tmp = (float)file_size / Pbyte;
         file_size_str = format_size_str(tmp, 1, "P");
-
     } else if (file_size >= Tbyte) {
-
         tmp = (float)file_size / Tbyte;
         file_size_str = format_size_str(tmp, 1, "T");
-
     } else if (file_size >= Gbyte) {
         tmp = (float)file_size / Gbyte;
         file_size_str = format_size_str(tmp, 1, "G");
-
     } else if (file_size >= Mbyte) {
         tmp = (float)file_size / Mbyte;
-
         file_size_str = format_size_str(tmp, 1, "M");
-
     } else if (file_size >= 1000) {
         tmp = (float)file_size / Kbyte;
         file_size_str = format_size_str(tmp, 1, "K");
-
     } else {
-
+        // For sizes less than 1000 bytes, display in bytes
         file_size_str = mx_itoa(file_size);
         mx_strcat(file_size_str, "B");
-
     }
     
-    return mx_strdup(file_size_str);
+    return mx_strdup(file_size_str); // Return the final formatted size string
 }
